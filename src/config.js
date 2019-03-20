8e9de942a8f7be3fccbeb90371e255372dbc7606
Kristina Maceković <macekovick@gmail.com>
2019-03-20 20:36:09 +0100

    Add config.js file, fetch products

---
+++ src/config.js
@@ -0,0 +1,10 @@
+/*
+ ** this file contains all the configurations we need
+ ** url for info: https://the-purple-brand-27.commercelayer.io/admin/settings/applications/571/edit
+ */
+
+export default {
+  clientId: "a23e052c6fddedd87b463ecbd6fe4599f0dad8ff12033f244fc3291f27393cf0",
+  scope: "market:533",
+  apiUrl: "https://the-purple-brand-27.commercelayer.io"
+};
